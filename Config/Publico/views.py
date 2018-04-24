fa8bdff6a7156fb14c7214a20b4c7ee0b27153b0
Santykax <soysantiagokaiser@gmail.com>
2018-04-24 11:52:51 -0300

    importe giladas en el views

--- Config/Publico/views.py
+++ Config/Publico/views.py
@@ -2,6 +2,20 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login as auth_login
+from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response, render, redirect
+from django.template import RequestContext
+from django.conf import settings
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import AnonymousUser, User
+from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
+from datetime import datetime, timedelta
+from django.utils import timezone
 from Publico.models import *
 
 def index(request):
